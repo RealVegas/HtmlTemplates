19ff2a673485f8dcff491c8d9c8aa1876c171ba7
Вадим Голов <grafit-design@inbox.ru>
2024-11-18 23:41:06 +0500

    Все готово

--- main.py
+++ main.py
@@ -2,9 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def index():
-    return render_template('index.html')
+def index() -> str:
+    context: dict[str, str] = {'home_active': 'active',
+                               'home_link': '/',
+                               'about_link': '/about/'}
+    return render_template('home.html', **context)
+
+
+@app.route('/about/')
+def about() -> str:
+    context: dict[str, str] = {'about_active': 'active',
+                               'home_link': '/',
+                               'about_link': '/about/'}
+    return render_template('about.html', **context)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
